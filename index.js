0ec3d02d3981c80552070405cd166615693bcf29
Will Wilson <94478894+willfwilson@users.noreply.github.com>
2023-10-04 21:40:05 +0100

    major update
    added images, video, content

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ if ((width >= 1000)){
     if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         $("#header").css("background","#000");
         $("#header").css("color","#fff");
-        $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
+        // $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
         $("#header").css("padding","4vh 4vw");
         $("#navigation a").hover(function(){
             $(this).css("border-bottom","2px solid rgb(255, 44, 90)");
@@ -14,7 +14,7 @@ if ((width >= 1000)){
     }else{
         $("#header").css("background","#000");
         $("#header").css("color","#000");
-        $("#header").css("box-shadow","0px 0px 0px rgba(0,0,0,0)");
+        // $("#header").css("box-shadow","0px 0px 0px rgba(0,0,0,0)");
         $("#header").css("padding","6vh 4vw");
         $("#navigation a").hover(function(){
             $(this).css("border-bottom","2px solid #000");
